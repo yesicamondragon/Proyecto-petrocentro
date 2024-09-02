d17765e4511a8f993cd47eafb75fabf2b9c2fdaf
dfvalencia <dfvalencia019@misena.edu.co>
2024-09-02 10:43:34 -0500

    prduccion

--- Petrocentro/settings.py
+++ Petrocentro/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
-from Petrocentro.settings_production import *
+from .settings_production import *
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 

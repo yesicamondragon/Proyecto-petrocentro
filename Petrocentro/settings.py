aae51c3eea5a6829ab22bbe0c2f689d5e99e00cc
dfvalencia <dfvalencia019@misena.edu.co>
2024-09-02 11:04:11 -0500

    Update settings.py

--- Petrocentro/settings.py
+++ Petrocentro/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'petrocentro',
         'USER': 'PTC-DB-01',
-        'PASSWORD': 'Ti.wt.24.',
+        'PASSWORD': 'Ti.wt.24@',
         'HOST': 'localhost',
         'PORT':'3306',
     }
